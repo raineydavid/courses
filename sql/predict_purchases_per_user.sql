491d26fc8c629c637c86d6e83e75f29b71137e42
Rainey <23113728+raineydavid@users.noreply.github.com>
2020-05-26 05:35:18 +0100

    Update predict_purchases_per_user.sql

--- sql/predict_purchases_per_user.sql
+++ sql/predict_purchases_per_user.sql
@@ -1,4 +1,19 @@
 #standardSQL
+/*
+You'll realize the SELECT and FROM portions of the query is identical to that used during training. 
+
+The WHERE portion reflects the change in time frame and the FROM portion shows that you're calling ml.EVALUATE.
+
+If used with a linear regression model, the above query returns the following columns:
+
+mean_absolute_error, mean_squared_error, mean_squared_log_error, median_absolute_error, r2_score, explained_variance. 
+
+If used with a logistic regression model, the above query returns the following columns:
+
+precision, recall accuracy, f1_score log_loss, roc_auc 
+
+Please consult the machine learning glossary or run a Google search to understand how each of these metrics are calculated and what they mean.
+*/
 SELECT
   fullVisitorId,
   SUM(predicted_label) as total_predicted_purchases
