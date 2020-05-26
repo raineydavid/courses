7a0831deca20061eb07a720e11ae02633a4bbdcc
Rainey <23113728+raineydavid@users.noreply.github.com>
2020-05-26 06:07:52 +0100

    Create predict_total_by_country.sql

---
+++ sql/predict_total_by_country.sql
@@ -0,0 +1,21 @@
+#standardSQL
+/* the country and the sum of purchases for each country, so that's why SELECT, GROUP BY and ORDER BY. 
+LIMIT is used to ensure you only get the top 10 results.
+*/
+SELECT
+  country,
+  SUM(predicted_label) as total_predicted_purchases
+FROM
+  ml.PREDICT(MODEL `bqml_lab.sample_model`, (
+SELECT
+  IFNULL(device.operatingSystem, "") AS os,
+  device.isMobile AS is_mobile,
+  IFNULL(totals.pageviews, 0) AS pageviews,
+  IFNULL(geoNetwork.country, "") AS country
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  _TABLE_SUFFIX BETWEEN '20170701' AND '20170801'))
+GROUP BY country
+ORDER BY total_predicted_purchases DESC
+LIMIT 10;
